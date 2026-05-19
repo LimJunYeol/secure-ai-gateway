2593c004f29bd93f31424c06734ce52f57fe02df
Adu <ojo2284@naver.com>
2026-05-19 14:57:39 +0900

    feat: initial Spring Boot project with hello endpoint

---
+++ gateway/src/main/java/com/aiagent/gateway/HelloController.java
@@ -0,0 +1,13 @@
+package com.aiagent.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Secure AI Gateway is alive!";
+    }
+}
